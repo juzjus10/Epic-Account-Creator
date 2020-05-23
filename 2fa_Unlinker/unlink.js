8af37dd3cf75e6e488a3c7fe37a117d7a5ffdbd7
Justine Malagar <juzjus10@gmail.com>
2020-05-24 04:01:54 +0800

    Added 2FA Unlinker

--- 2fa_Unlinker/unlink.js
+++ 2fa_Unlinker/unlink.js
@@ -5,6 +5,7 @@ const {
   until
 } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
+const chromedriver = require('chromedriver');
 const MailosaurClient = require('mailosaur');
 const cheerio = require('cheerio')
 const fs = require('fs');
@@ -16,21 +17,22 @@ let driver;
 var email;
 var password;
 var account;
-var civ6account;
+var unlinked_accounts;
 var oldacc;
-var contents = fs.readFileSync("credentials.json").toString()
+var contents = fs.readFileSync("../credentials.json").toString()
 var credentials = JSON.parse(contents)
 
 const client = new MailosaurClient(credentials.apikey);
 const SERVER_ID = credentials.serverId;
 
 
+
 register();
 
 function register() {
 
-  civ6account = fs.readFileSync("civ6_accounts.txt").toString();
-  oldacc = fs.readFileSync("accounts.txt").toString();
+  unlinked_accounts = fs.readFileSync("unlinked_accounts.txt").toString();
+  oldacc = fs.readFileSync("existing_accounts.txt").toString();
   oldacc = oldacc.split(/\s*[\n:]+\s*/);
 
   for (i = credentials.accountctr; i < oldacc.length; i++) {
@@ -63,41 +65,33 @@ function register() {
       await driver.wait(until.elementIsEnabled(butones), 150000);
       await butones.click();
 
-      twoFAcode = await verify2FA(date);
-      await console.log(twoFAcode);
-      await driver.findElement(By.name('code')).sendKeys(twoFAcode);
+      await verify2FA(date);
 
-      var continueBtn = await driver.findElement(By.id('continue'));
-      await driver.wait(until.elementIsEnabled(continueBtn), 150000);
-      await continueBtn.click();
-      await driver.sleep(1300);
-      await driver.get('https://www.epicgames.com/store/purchase?namespace=cd14dcaa4f3443f19f7169a980559c62&showNavigation=true&highlightColor=0078f2&offers=fe74b3dad04846e5a58f62aebd3858b6');
-      await driver.wait(until.elementLocated(By.xpath("//*[@id='purchase-app']/div/div[4]/div[1]/div[2]/div[5]/div/div/button/span")), 150000);
+
+
+      await driver.get('https://www.epicgames.com/account/password');
+      await driver.wait(until.titleIs('Change Your Password'), 150000);
+      await driver.sleep(3000)
+      await driver.executeScript('var btn = document.getElementsByClassName("email-auth"); btn[0].click();')
+      await driver.wait(until.elementLocated(By.className('common-disable-container')), 150000);
+      await driver.executeScript('var btn1 = document.getElementsByClassName("proceed-btn"); btn1[0].click();')
       await driver.sleep(1000);
-      await driver.findElement(By.xpath("//*[@id='purchase-app']/div/div[4]/div[1]/div[2]/div[5]/div/div/button/span")).click();
-      await driver.sleep(1000);
+    //  await verify2FA(date);
 
-      try {
-        await driver.wait(until.elementLocated(By.xpath("//*[@id='purchase-app']/div/div[4]/div[1]/div[2]/div[6]/div[2]/div/div[2]/button[2]")), 2500);
-        await driver.findElement(By.xpath("//*[@id='purchase-app']/div/div[4]/div[1]/div[2]/div[6]/div[2]/div/div[2]/button[2]")).click();
-      } catch (e) {
-        var foo = 'bar';
-      }
 
-      await driver.wait(until.elementLocated(By.className('receipt-container')), 100000);
 
       account = '\n' + email + ":" + password;
-      fs.writeFileSync("civ6_accounts.txt", civ6account + account)
+      fs.writeFileSync("unlinked_accounts.txt", unlinked_accounts + account)
 
       credentials.accountctr += 2;
-      fs.writeFileSync("credentials.json", JSON.stringify(credentials))
+      fs.writeFileSync("../credentials.json", JSON.stringify(credentials))
 
 
     } //try block
     catch (e) { // catch block
       console.log(e);
       account = email + ":" + password + "(incomplete)\n";
-      fs.writeFileSync("civ6_accounts.txt", civ6account + account)
+      fs.writeFileSync("unlinked_accounts.txt", unlinked_accounts + account)
     } finally {
       await driver.manage().deleteAllCookies()
       await driver.close();
@@ -124,6 +118,11 @@ async function verify2FA(date) {
     let message = await client.messages.getById(twofaid);
     const $ = cheerio.load(message.html.body)
     var result = $('body > table > tbody > tr > td > center > table > tbody > tr > td > table:nth-child(4) > tbody > tr > td > table > tbody > tr > td > table > tbody > tr:nth-child(3) > td > div').text().trim();
+    await console.log(result);
+    await driver.findElement(By.name('code')).sendKeys(result);
+    var continueBtn = await driver.findElement(By.id('continue'));
+    await driver.wait(until.elementIsEnabled(continueBtn), 150000);
+    await continueBtn.click();
     return result;
   } catch (e) {
     console.log(e);
---
+++ archive/gtav.js
@@ -0,0 +1,189 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const MailosaurClient = require('mailosaur');
+const cheerio = require('cheerio')
+const fs = require('fs');
+
+var res = '';
+var verificationcode = '';
+var twoFAcode = '';
+var twofaid = '';
+let driver;
+var email;
+var account;
+var oldacc;
+var contents = fs.readFileSync("credentials.json").toString()
+var credentials = JSON.parse(contents)
+
+const client = new MailosaurClient(credentials.apikey);
+const SERVER_ID = credentials.serverId;
+
+
+register();
+
+function register(){
+
+  oldacc = fs.readFileSync("accounts.txt").toString();
+  email = credentials.username + credentials.usernamectr + credentials.email;
+
+
+  (async () => {
+
+  var options = new chrome.Options();
+  options.addArguments('disable-gpu');
+  options.addArguments("--lang=en-US");
+
+  try{
+    driver = new Builder().forBrowser('chrome').setChromeOptions(options).build();
+	  var date = new Date(Date.now())
+    await driver.get('https://www.epicgames.com/id/register');
+    await   driver.wait(until.titleIs('Register for an Epic Games account | Epic Games'), 5000);
+
+    if (driver.wait(until.elementLocated(By.id('login-with-epic')), 150000)) {
+      await   driver.findElement(By.id('login-with-epic')).click();
+    }
+    await   driver.findElement(By.name('name')).sendKeys(credentials.name);
+    await   driver.findElement(By.name('lastName')).sendKeys(credentials.lastName);
+    await   driver.findElement(By.name('displayName')).sendKeys(credentials.username + credentials.usernamectr);
+    await   driver.findElement(By.name('email')).sendKeys(email);
+    await   driver.findElement(By.name('password')).sendKeys(credentials.password);
+    await   driver.findElement(By.name('termsOfService')).click();
+    var  butones = await  driver.findElement(By.className('MuiButton-fullWidth'));
+    await driver.wait(until.elementIsEnabled(butones), 150000);
+    await  butones.click();
+
+
+    await getEmail(date);
+
+    await driver.wait(until.titleIs('Personal Details'), 150000);
+    await driver.get('https://www.epicgames.com/account/password');
+    await driver.wait(until.titleIs('Change Your Password'), 150000);
+    await driver.sleep(3000)
+    await driver.executeScript('var titi = document.getElementsByClassName("email-auth"); titi[0].click();')
+    await driver.wait(until.elementLocated(By.className('inner-container')), 150000);
+
+    await get2faEmail(date);
+
+
+ } //try block
+  catch (e){
+    console.log(e);
+
+
+  } //catch block
+
+  finally {
+    ++credentials.usernamectr;
+    fs.writeFileSync("credentials.json", JSON.stringify(credentials))
+
+  }
+
+
+
+   })();
+}
+
+
+
+
+function getEmail(date) {
+
+  client.messages.search(SERVER_ID, {
+    subject: "Epic Games - Email Verification"
+  }, {
+    receivedAfter: date,
+    itemsPerPage: 200,
+    timeout: 60000
+  }).then((results) => {
+
+
+    res= results.items[0].id;
+    getVerificationCode ();
+
+  });
+
+}
+
+function getVerificationCode () {
+console.log("Getting Email Verification Code ....");
+  (async () => {
+
+
+    let message = await client.messages.getById(res);
+    const $ = cheerio.load(message.html.body)
+    verificationcode = $('body > table > tbody > tr > td > center > table:nth-child(2) > tbody > tr > td > table > tbody > tr > td > table > tbody > tr:nth-child(3) > td > div').text().trim();
+    await console.log("Verification Code: " + verificationcode);
+    await driver.wait(until.elementLocated(By.name('code')), 150000);
+    await driver.findElement(By.name('code')).sendKeys(verificationcode);
+    await driver.findElement(By.id('continue')).click();
+    return await verificationcode;
+
+
+  })();
+}
+
+function get2faEmail(date) {
+  console.log("Getting 2FA Email Code .....");
+  client.messages.search(SERVER_ID, {
+    subject: "Your two-factor sign in code"
+  }, {
+   receivedAfter:  date,
+    itemsPerPage: 200,
+    timeout: 60000
+  }).then((results) => {
+  twofaid =  results.items[0].id;
+
+ verify2FA();
+});
+}
+
+function verify2FA (){
+  (async () => {
+    try {
+      let message = await client.messages.getById(twofaid);
+      const $ = cheerio.load(message.html.body)
+      var twoFAcode = $('body > table > tbody > tr > td > center > table > tbody > tr > td > table:nth-child(4) > tbody > tr > td > table > tbody > tr > td > table > tbody > tr:nth-child(3) > td > div').text().trim();
+      await console.log(twoFAcode);
+      await driver.findElement(By.name('challengeEmailCode')).sendKeys(twoFAcode);
+      await driver.findElement(By.className('proceed-btn')).click();
+      var donebtn =  await driver.wait(until.elementLocated(By.className('done-btn')), 150000);
+      await donebtn.click();
+      await driver.get('https://launcher-website-prod07.ol.epicgames.com/purchase?showNavigation=true&namespace=0584d2013f0149a791e7b9bad0eec102&offers=954871df36d3456ca1face43aa5c2e62');
+/*
+      await driver.get('https://www.epicgames.com/store/en-US/product/grand-theft-auto-v/home');
+      await driver.wait(until.titleIs('Grand Theft Auto V - Grand Theft Auto V: Premium Edition'), 150000);
+      await driver.wait(until.elementLocated(By.className('Button-dark_c0429b3d')), 150000);
+      await driver.findElement(By.className('Button-dark_c0429b3d')).click();
+
+      var getButton = await driver.findElement(By.className('PurchaseButton-button_d3bea90e'));
+      await driver.executeScript("arguments[0].scrollIntoView({behavior: 'auto', block: 'center'})", getButton);
+      getButton.click(); */
+      await driver.wait(until.elementLocated(By.xpath("//*[@id='purchase-app']/div/div[4]/div[1]/div[2]/div[5]/div/div/button/span")), 150000);
+      await driver.sleep(1000);
+      await driver.findElement(By.xpath("//*[@id='purchase-app']/div/div[4]/div[1]/div[2]/div[5]/div/div/button/span")).click();
+      try {
+	await driver.wait(until.elementLocated(By.xpath("//*[@id='purchase-app']/div/div[4]/div[1]/div[2]/div[6]/div[2]/div/div[2]/button[2]")), 2500);	  
+	await driver.findElement(By.xpath("//*[@id='purchase-app']/div/div[4]/div[1]/div[2]/div[6]/div[2]/div/div[2]/button[2]")).click();
+	  } catch (e) { var foo = 'bar'; }
+      await driver.wait(until.elementLocated(By.className('receipt-container')), 100000);
+
+      account = email + ":" + credentials.password + '\n';
+      fs.writeFileSync("accounts.txt", oldacc + account)
+
+      return await twoFAcode;
+    }
+    catch (e){
+      console.log(e);
+      account = email + ":" + credentials.password + "(incomplete)\n";
+      fs.writeFileSync("accounts.txt", oldacc + account)
+    }
+    finally
+    {
+      await driver.manage().deleteAllCookies()
+      await driver.close();
+      await register();
+    }
+
+
+      })();
+}
